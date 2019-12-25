8b9fafeacd214e85e9fc47cac38888fe7f4f37b7
A.Unger <zyxancf@gmail.com>
2019-12-25 17:52:00 +0100

    some comments

--- pkg/multicast/server.go
+++ pkg/multicast/server.go
@@ -1,3 +1,4 @@
 package multicast
 
 // Multicast provides a service that listens on a multicast group
+// One should be able to start `n` servers / listeners, send a message to the UDP Multicast address and have all servers responding.
