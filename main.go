0e8495fe15596b68c389ab3e19a464f279b61400
A.Unger <zyxancf@gmail.com>
2019-12-26 18:33:57 +0100

    some preparations

--- main.go
+++ main.go
@@ -1,35 +1,44 @@
 package main
 
-import (
-	"net"
-	"os"
-	"time"
-
-	"github.com/refs/mdns/pkg/server"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-)
-
 func main() {
-	addr, err := net.ResolveUDPAddr("udp", ":1054") // TODO: why does :1054 work but `localhost:1054` or `127.0.0.1:1054` don't?
-	if err != nil {
-		log.Err(err)
-		os.Exit(1)
-	}
+	// get a network interface and print it
+	// in preparation for listening multicast:
+	// func ListenMulticastUDP(network string, ifi *Interface, gaddr *UDPAddr) (*UDPConn, error)
 
-	s := server.Server{
-		Addr: *addr,
-		Log:  NewLogger(1),
-	}
-
-	if err := s.Start(); err != nil {
-		log.Err(err)
-		os.Exit(1)
-	}
 }
 
-// NewLogger is a utility function
-func NewLogger(level zerolog.Level) zerolog.Logger {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	return zerolog.New(output).With().Timestamp().Logger().Level(level)
-}
+// package main
+
+// import (
+// 	"net"
+// 	"os"
+// 	"time"
+
+// 	"github.com/refs/mdns/pkg/server"
+// 	"github.com/rs/zerolog"
+// 	"github.com/rs/zerolog/log"
+// )
+
+// func main() {
+// 	addr, err := net.ResolveUDPAddr("udp", ":1054") // TODO: why does :1054 work but `localhost:1054` or `127.0.0.1:1054` don't?
+// 	if err != nil {
+// 		log.Err(err)
+// 		os.Exit(1)
+// 	}
+
+// 	s := server.Server{
+// 		Addr: *addr,
+// 		Log:  NewLogger(1),
+// 	}
+
+// 	if err := s.Start(); err != nil {
+// 		log.Err(err)
+// 		os.Exit(1)
+// 	}
+// }
+
+// // NewLogger is a utility function
+// func NewLogger(level zerolog.Level) zerolog.Logger {
+// 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+// 	return zerolog.New(output).With().Timestamp().Logger().Level(level)
+// }
